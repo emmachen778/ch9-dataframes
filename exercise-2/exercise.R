d62955828345d2e1c8c94fad31ddd047e4335380
Emma Chen <32378909+emmapjchen@users.noreply.github.com>
2017-10-12 15:05:21 -0700

    Complete exercise 2 and 4

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,51 +1,51 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- paste('Employee', 1:100)
 
 # Create a vector of 2014 salaries using the runif function
-
+salaries.2014 <- runif(100, 40000, 50000)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salaries.2015 <- salaries.2014 + runif(100, -5000, 10000)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, salaries.2014, salaries.2015, stringsAsFactors = F)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries$salaries.2015 - salaries$salaries.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salary.57 <- salaries[salaries$employees == 'Employee 57', 'salaries.2015']
 
 # How many employees got a raise?
-
+num.raised <- nrow(salaries[salaries$got.raise == TRUE,])
 
 # What was the value of the highest raise?
-
+highest.raise <- max(salaries$raise)
 
 # What was the name of the employee who recieved the highest raise?
-
+got.highest.raise <- salaries[salaries$raise == highest.raise, 'employees']
 
 # What was the largest decrease in salaries between the two years?
-
+largest.cut <- min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+got.largest.cut <- salaries[salaries$raise == largest.cut, 'employees']
 
 # What was the average salary increase?
-
+average <- mean(salaries$raise)
 
 ### Bonus ###
 
 # Write a .csv file of your salaries to your working directory
-
+write.csv(salaries, "salaries.csv")
 
 # For people who did not get a raise, how much money did they lose?
-
+avg.loss <- mean(salaries$raise[salaries$got.raise == FALSE])
 
 # Is that what you expected them to lose?
 
